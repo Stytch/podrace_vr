ed007502a63799a7b79b9e483cc6ac1f7c1f8ccc
jradisson@seven-shapes.com <thespacemustache@gmail.com>
2019-11-29 11:16:34 +0100

    Changes on Menu, added anims (still needs work), added controller scripts

---
+++ LW_PodRacer/Assets/Scripts/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Button PlayButton;
+    public Button OptionButton;
+    public Button QuitButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayButton()
+    {
+        //On lance le jeu
+        Debug.Log("PlayButton Pressed");
+    }
+
+    public void OnOptionButton()
+    {
+        //On ouvre le menu des options
+        Debug.Log("OptionButton Pressed");
+    }
+
+    public void OnQuitButton()
+    {
+        Debug.Log("QuitButton Pressed");
+        Application.Quit();
+    }
+}
