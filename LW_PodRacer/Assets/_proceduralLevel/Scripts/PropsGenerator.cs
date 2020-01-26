4c06ae3f2371c15b36d9fbb56b653f09f648f137
nicolas-raymond <37674067+nicolas-raymond@users.noreply.github.com>
2020-01-26 18:08:35 +0100

    Maj: Generate Props

--- LW_PodRacer/Assets/_proceduralLevel/Scripts/PropsGenerator.cs
+++ LW_PodRacer/Assets/_proceduralLevel/Scripts/PropsGenerator.cs
@@ -6,12 +6,22 @@ public class PropsGenerator : MonoBehaviour
 {
     //VARIABLE
     private TrackGenerator tg;
-    private GameObject allProps;
+    private MeshGenerator mg;
+
+    [HideInInspector]
+    public GameObject allProps;
 
     public bool debugMode = false;
 
     //path
-    private string pathRock = "Prefab/props/rock/rock";
+    private string pathRock = "Assets/Blender/rock_final/rock Variant.prefab";
+
+    //PREFAB
+    public GameObject rock;
+    public GameObject tower;
+    public GameObject house;
+    public GameObject ark;
+
     //FUNCTION
     //INIT VARIABLE
     bool IniVariable()
@@ -19,16 +29,19 @@ public class PropsGenerator : MonoBehaviour
         tg = transform.GetComponent<TrackGenerator>();
         if (tg == null) return false;
 
+        mg = transform.GetComponent<MeshGenerator>();
+        if (mg == null) return false;
+
         return true;
     }
 
-    private GameObject InstanceProps(Vector3 pos, float yRot, Vector3 sca)
+    private GameObject InstanceProps(GameObject go, Vector3 pos, float yRot, Vector3 sca)
     {
-        GameObject instance = Instantiate(Resources.Load(pathRock, typeof(GameObject))) as GameObject;
+        GameObject instance = Instantiate(go) as GameObject;
         instance.transform.position = pos;
-        instance.transform.eulerAngles += new Vector3(0, yRot, 0);
+        instance.transform.eulerAngles = new Vector3(0, yRot, 0);
         instance.transform.localScale = sca;
-
+        instance.transform.parent = allProps.transform;
         return instance;
     }
 
@@ -48,18 +61,31 @@ public class PropsGenerator : MonoBehaviour
             if (allProps != null) Destroy(allProps);
             allProps = new GameObject();
             allProps.name = "AllProps";
+            if (mg.allTrackMesh != null) allProps.transform.parent = mg.allTrackMesh.transform;
 
             foreach (Track_part t in tg.all_tracks)
             {
+
+                //ARCHE
+                int ind = Random.Range(-10 * t.points.Count, t.points.Count);
+                if (ind >= 0)
+                {
+                    Point point = t.points[ind];
+
+                    GameObject a = InstanceProps(ark, point.position, 0.0f, new Vector3(3, 3, 3));
+                    a.transform.forward = point.tangent;
+                }
+
+                //ROCHE
                 foreach (Point p in t.points)
                 {
-                    int test = Random.Range(0, 10000);
+                    int test = Random.Range(0, 1000);
                     if (test < 50)
                     {
                         float sca = Random.Range(0.1f, 2f);
                         Vector3 pos = p.position + p.normal * Random.Range(0.1f, 10.0f) + Vector3.down *3;
-                        GameObject rock = InstanceProps(pos, Random.Range(0, 360), new Vector3(sca, sca, sca));
-                        rock.transform.parent = allProps.transform;
+                        GameObject r= InstanceProps(rock,pos, Random.Range(0, 360), new Vector3(sca, sca * Random.Range(1,4), sca));
+                        
                     }
                 }
             }
