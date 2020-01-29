e4c73a61456ae48206750da5a60c66c7316bc543
Your Name <legrand.remi14@gmail.com>
2020-01-29 21:09:55 +0100

    tweak du pod 4

--- LW_PodRacer/Assets/GameManager.cs
+++ LW_PodRacer/Assets/GameManager.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("SFX/MANAGEMENT")]
     public AudioSource as_Music;
     public AudioSource as_InterfaceSound;
     public AudioClip[] ac_Clips;
+    [Header("GAM/GAME OBJECTS")]
     public GameObject gop_Podracer;
     public Transform t_spawnPodracer;
     public Transform t_spawnTempete;
-    public GameObject go_tempete;
+    public Tempete m_tempete;
     // PODRACER - PLAYER
     private GameObject go_Podracer;
     private SFX_Controller m_Podracer;
     // GAME - STATUS
     private Coroutine cor_gameCoroutine;
     bool gameAlive = false;
+    [Header("UI/PANELS")]
     // GAME - STATUS
     public GameObject panel_win;
     public GameObject panel_loose;
     public GameObject panel_mainmenu; //attente de jules...
     //
-    public GameObject camera;
+    [Header("UI/CAMERA")]
+    public GameObject cam_mainmenu;
+    public GameObject cam_podracer;
+    [Header("GAM/TRACK GENERATOR")]
+    public TrackManager m_trackManager; // attente full integration procedural finoux
 
     void Start()
     {
@@ -33,21 +40,20 @@ public class GameManager : MonoBehaviour
     public void startupbtn()
     {
         print("startupbtn");
-        resetGame();
-        cor_gameCoroutine = StartCoroutine(StartNewGame());
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.P) && !gameAlive)
+        if (!gameAlive)
         {
             resetGame();
             cor_gameCoroutine = StartCoroutine(StartNewGame());
         }
-        if (Input.GetKeyDown(KeyCode.P) && gameAlive)
+        else
         {
             endGame(GameEndStatus.abandon);
         }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) startupbtn();
     }
 
     public void generatePlayerPodracer()
@@ -55,18 +61,21 @@ public class GameManager : MonoBehaviour
         go_Podracer = Instantiate(gop_Podracer, t_spawnPodracer.position, t_spawnPodracer.rotation);
         m_Podracer = go_Podracer.GetComponent<SFX_Controller>();
         m_Podracer.m_gamemanager = this;
+        cam_podracer = go_Podracer.GetComponentInChildren<Camera>().gameObject;
+        m_Podracer.m_tempete = m_tempete;
         // TODO : ATTACH CAMERA ?
     }
     public void destroyPlayerPodRacer()
     {
         // TODO : DETACH CAMERA ?
         Destroy(go_Podracer);
+        m_Podracer = null;
+        cam_podracer = null;
     }
     public void endGame(GameEndStatus status)
     {
         if (gameAlive)
         {
-            camera.SetActive(true);
             switch (status)
             {
                 case GameEndStatus.win:
@@ -80,8 +89,10 @@ public class GameManager : MonoBehaviour
             }
 
             //PRIMODIAL
+            cam_mainmenu.SetActive(true);
             panel_mainmenu.SetActive(true);
             gameAlive = false;
+            m_tempete.moveEnabled = false;
         }
     }
 
@@ -89,12 +100,14 @@ public class GameManager : MonoBehaviour
     {
         gameAlive = true;
         print("START NEW GAME");
-
+        m_tempete.moveEnabled = true;
         //SETUP GAME
         closeEndGamePanel();
         panel_loose.SetActive(false);
+        cam_mainmenu.SetActive(false);
+        panel_mainmenu.SetActive(false);
         generatePlayerPodracer();
-        camera.SetActive(false);
+        cam_podracer.SetActive(true);
         //
         while (gameAlive)
         {
@@ -103,12 +116,15 @@ public class GameManager : MonoBehaviour
         destroyPlayerPodRacer();
         cor_gameCoroutine = null;
         gameAlive = false;
+        m_tempete.moveEnabled = false;
+
     }
 
     // pour plus tard...
     public void resetGame()
     {
-        go_tempete.transform.position = t_spawnTempete.position;
+        endGame(GameEndStatus.abandon);
+        m_tempete.transform.position = t_spawnTempete.position;
         cor_gameCoroutine = null;
         gameAlive = false;
     }
---
+++ LW_PodRacer/Assets/Tempete.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tempete : MonoBehaviour
+{
+    public float moveSpeed = 1000f;
+    public bool moveEnabled = false;
+    void Update()
+    {
+        if (moveEnabled)
+        {
+            var pos = transform.position;
+            pos.z += Time.deltaTime * moveSpeed;
+            transform.position = pos;
+        }
+    }
+}
